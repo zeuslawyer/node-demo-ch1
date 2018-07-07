a0863b1b9d14b2c6aab200fc35eefab6370cac73
Zeuslawyer <zubin.pratap@gmail.com>
2018-07-07 10:49:45 +1000

    install pug. configure app.js settings to include. create views folder with form.pug. WORKING

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('path');
 const routes = require('./routes/index');
 
 const app = express();
+//configure settings to include pug & views folder path
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use('/', routes);
 
