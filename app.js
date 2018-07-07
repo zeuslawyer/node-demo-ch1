ffb7b279b62509706982b53cc89c90f235364514
Zeuslawyer <zubin.pratap@gmail.com>
2018-07-07 12:50:44 +1000

    add body-parser. add POST handler. pug indentation is a PAIN. WORKING.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const routes = require('./routes/index');
 
 const app = express();
@@ -7,6 +8,8 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//middleware 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', routes);  // this refers to ROUTES folder which contains all the routes handlers/middleware. encapsulates & modularises
 
 
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,18 @@ const router = express.Router();
 
 
 //middleware aka handler for the default index '/' path aka route
+
+//get
 router.get('/', (req, res, nex) => {
-    res.render('form',  {title: 'Registration Form' }); // render the file layout in the /views folder
+    res.render('form',  {title: 'Registration',  }); // render the file layout called 'form' in the /views folder
 });
+
+//post
+router.post('/', (req, res, next) => {
+    console.log(req.body)
+    res.render('form', {title: 'Resistration -POST'} );
+});
+
+
 
 module.exports = router;
