2d562b3f931cd83df2b27453d9ff5efd9a17dd73
Zeuslawyer <zubin.pratap@gmail.com>
2018-07-07 22:24:53 +1000

    add express-validator and do name and email validations. added parking form html and router in routes/index. WORKING

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const routes = require('./routes/index');
 
+
+
 const app = express();
-//configure settings to include pug & views folder path
+
+//configure settings to include pug & views folder path  - see also https://expressjs.com/en/guide/using-template-engines.html
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 //middleware 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use('/', routes);  // this refers to ROUTES folder which contains all the routes handlers/middleware. encapsulates & modularises
+app.use('/', routes);  // this refers to index.js in ROUTES folder which contains  the routing handler/middleware 
 
 
 module.exports = app;
 
 
+
+// LESSON REFERENCE: https://www.sitepoint.com/build-simple-beginner-app-node-bootstrap-mongodb/
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const {body, validationResult} = require('express-validator/check');
 
 
 //middleware aka handler for the default index '/' path aka route
@@ -9,11 +10,37 @@ router.get('/', (req, res, nex) => {
     res.render('form',  {title: 'Registration',  }); // render the file layout called 'form' in the /views folder
 });
 
-//post
-router.post('/', (req, res, next) => {
-    console.log(req.body)
-    res.render('form', {title: 'Resistration -POST'} );
+//get
+router.get('/parking', (req, res, nex) => {
+    res.sendFile('/Users/zubinpratap/Documents/DevProjects/Practice_Projects/WebDevTutes/node-demo-ch1/views/ParkingForm.html'); 
 });
+
+//post
+router.post('/',
+  //apply conditions on the req.body's two properties - name and email - with this array of handler methods from express-validator
+  [
+    body('name')
+      .isLength({ min: 5 })
+      .withMessage(`Please enter a name that is at least 5 letters long`),
+    body('email')
+      .isLength({ min: 1 })
+      .withMessage('Please enter an email'),
+  ],
+  // handler that then uses express-validator methods to validate
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+      res.send('Thank you for your registration!');
+    } else {
+      res.render('form', {
+        title: 'Registration form',
+        errors: errors.array(),
+        data: req.body,
+      });
+    }
+  }
+);
 
 
 
